c50aeece0447801245a2f9ea73b12fac9d968f78
WNing <khongwn06@gmail.com>
2026-03-07 01:31:38 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+double test_num(int a, int b){
+	int sum=a+b;
+	int sub=a-b;
+	int mul=a*b;
+	double div=(double)a/b;
+	
+	cout<<"\nThe relationship for A and B : "<<endl;
+	cout<<"Addtion : "<<sum<<"\nSubtraction : "<<sub<<"\nMultiplication : "<<mul<<"\nDivision : "<<div<<endl;
+	
+	return 1;
+}
+
+int main(){
+	int a, b;
+	
+	cout<<"Enter a number for A : ";
+	cin>>a;
+	cout<<"Enter a number for B : ";
+	cin>>b;
+	
+//	int sum=a+b;
+//	int sub=a-b;
+//	int mul=a*b;
+//	int div=a/b;
+	
+	test_num(a,b);
+	
+//	cout<<"\nThe relationship for A and B : "<<endl;
+//	cout<<"Addtion : "<<sum<<"\nSubtraction : "<<sub<<"\nMultiplication : "<<mul<<"\nDivision : "<<div<<endl;
+	
+	return 0;
+}
